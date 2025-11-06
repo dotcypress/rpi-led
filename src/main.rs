2b948ffc9ce91b7fb7b2353966a9b6f318f7ae54
Vitaly Domnikov <dotcypress@gmail.com>
2025-11-06 08:47:21 +0200

    GRB support

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         .nth(1)
         .and_then(|hex| u32::from_str_radix(&hex, 16).ok())
     {
-        let color = RGB8::new((color >> 16) as u8, (color >> 8) as u8, color as u8);
+        let r = (color >> 16) as u8;
+        let g = (color >> 8) as u8;
+        let b = color as u8;
+        #[cfg(not(feature = "grb"))]
+        let color = RGB8::new(r, g, b);
+        #[cfg(feature = "grb")]
+        let color = RGB8::new(g, r, b);
         let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 4_200_000, Mode::Mode0)?;
         Ws2812::new(spi).write([color])?;
     }
